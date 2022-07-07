e126fd84f417cdc4b5abf0f3f82505fbc4e39887
Jeong Jinwoo <jwjeong96@gmail.com>
2022-07-07 13:47:46 +0900

    CORS

--- frontend/src/apis/index.ts
+++ frontend/src/apis/index.ts
@@ -4,4 +4,7 @@ export const BASE_URL = 'http://localhost:8080/api/demo';
 
 export const client = axios.create({
   baseURL: BASE_URL,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
 });
